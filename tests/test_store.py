e1e07b383e2587c9fffe609543d83080423372b4
Nikita-QA-AT <olkin.nikita777@gmail.com>
2025-10-12 21:44:06 +0300

    Добавлена фикстура "create_order" в conftest.py + добавлен тест "Получение информации о заказе по ID (GET /store/order/{orderId})"

--- tests/test_store.py
+++ tests/test_store.py
@@ -3,6 +3,8 @@ import pytest
 import requests
 import jsonschema
 
+from tests.conftest import create_pet
+
 BASE_URL = "http://5.181.109.28:9090/api/v3"
 
 
@@ -34,3 +36,19 @@ class TestStore:
             assert response_json['status'] == payload['status'], f"status не совпадает с ожидаемым"
             assert response_json['complete'] == payload['complete'], f"complete не совпадает с ожидаемым"
 
+
+    @allure.title("Получение информации о заказе по ID (GET /store/order/{orderId})")
+    def test_get_order_by_id(self, create_order):
+        with allure.step("Отправка запроса на получение информации о заказе по ID"):
+            response = requests.get(url=f"{BASE_URL}/store/order/1")
+            response_json = response.json()
+
+        with allure.step("Проверка статуса ответа"):
+            assert response.status_code == 200, f"Ожидаю код 200, а получил {response.status_code}"
+
+        with allure.step("Проверка параметров заказа в ответе"):
+            assert response_json['id'] == create_order['id'], f"id заказа не совпадает с ожидаемым"
+            assert response_json['petId'] == create_order['petId'], f"petId заказа не совпадает с ожидаемым"
+            assert response_json['quantity'] == create_order['quantity'], f"quantity заказа не совпадает с ожидаемым"
+            assert response_json['status'] == create_order['status'], f"status заказа не совпадает с ожидаемым"
+            assert response_json['complete'] == create_order['complete'], f"complete заказа не совпадает с ожидаемым"
